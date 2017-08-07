96739a1216fb5fa67680ef73a12eb02760a09fc9
Marcus Rocha <sif.marcusvinicius@gmail.com>
2017-08-07 19:38:28 +0000

    configuração inicial do servidor

---
+++ app.js
@@ -0,0 +1,9 @@
+/* configura'ões do servidor */
+
+var app = require('./config/server');
+
+/* parametrizar porta de escuta */
+
+app.listen(3000, function(){
+	console.log('Servidor Online');
+})
---
+++ config/server.js
@@ -0,0 +1,37 @@
+/* importar o módulo do framework express */
+var express = require('express');
+
+/* importar o módulo do framework consign */
+var consign = require('consign');
+
+/* importar o módulo do framework bodyParser */
+var bodyParser = require('body-parser');
+
+/* importar o módulo do framework expressValidator */
+var expressValidator = require('express-validator');
+
+/* iniciar o objeto do express */
+var app = express();
+
+/* star as variáveis 'view engine' e 'views' do express */
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+/* configurar o middleware express.static */
+app.use(express.static('./app/public'));
+
+/* configurar o middleware body-parser */
+app.use(bodyParser.urlencoded({extended: true}));
+
+/* configurar o middleware body-express-validator */
+app.use(expressValidator());
+
+/* efetua o auto load das rotas, dos models e dos controllers para o obejto app */
+consign()
+	.include('app/routes')
+	.then('app/models')
+	.then('app/controllers')
+	.into(app);
+
+/* exportar o objeto app */
+module.exports = app;
